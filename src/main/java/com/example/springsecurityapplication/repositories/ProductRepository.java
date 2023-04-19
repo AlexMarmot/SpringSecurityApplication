192a34186c0f8d7bed5136daea6a2d30adfd7c22
SurkovAleshka <baks123321@yandex.ru>
2023-04-19 12:22:26 +0300

    Добавлен сервисный слой товара, в котором реализованы методы по возданию, обновлению и удалению товара, а также выводу всех товаров или определенного товара по его id

--- src/main/java/com/example/springsecurityapplication/repositories/ProductRepository.java
+++ src/main/java/com/example/springsecurityapplication/repositories/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.example.springsecurityapplication.repositories;
 
-public interface ProductRepository {
+import com.example.springsecurityapplication.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
 }
